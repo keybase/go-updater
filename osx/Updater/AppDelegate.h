ee08322b7d1b1176c5d489a8293f27c6ffa177f9
Gabriel Handford <gabrielh@gmail.com>
2016-04-13 17:59:20 -0700

    App to show update prompt dialog on OS X
    Has no dependencies other than Cocoa.

---
+++ osx/Updater/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Updater
+//
+//  Created by Gabriel on 4/7/16.
+//  Copyright © 2016 Gabriel Handford. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@end
+
