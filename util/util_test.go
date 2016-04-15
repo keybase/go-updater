6df7c98064f4fb637d1ce70f06143531e7faa4d2
Gabriel Handford <gabrielh@gmail.com>
2016-04-14 17:21:13 -0700

    Util (v2) (#11)

---
+++ util/util_test.go
@@ -0,0 +1,31 @@
+// Copyright 2015 Keybase, Inc. All rights reserved. Use of
+// this source code is governed by the included BSD license.
+
+package util
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestJoinPredicate(t *testing.T) {
+	f := func(s string) bool { return strings.HasPrefix(s, "f") }
+	s := JoinPredicate([]string{"foo", "bar", "faa"}, "-", f)
+	if s != "foo-faa" {
+		t.Errorf("Unexpected output: %s", s)
+	}
+}
+
+func TestRandString(t *testing.T) {
+	s, err := RandString("prefix", 20)
+	t.Logf("Rand string: %s", s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !strings.HasPrefix(s, "prefix") {
+		t.Errorf("Invalid prefix: %s", s)
+	}
+	if len(s) != 38 {
+		t.Errorf("Invalid length: %s (%d)", s, len(s))
+	}
+}
