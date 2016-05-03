b2271fdf2027148aa2af6e8d0f4e96706848ca26
Gabriel Handford <gabrielh@gmail.com>
2016-05-03 15:34:18 -0700

    Request id
    Removing installId from options since its part of update response and config.

--- updater_test.go
+++ updater_test.go
@@ -43,6 +43,7 @@ type testUpdateUI struct {
 	errReported        error
 	actionReported     UpdateAction
 	autoUpdateReported bool
+	updateReported     *Update
 }
 
 func (u testUpdateUI) UpdatePrompt(_ Update, _ UpdateOptions, _ UpdatePromptOptions) (*UpdatePromptResponse, error) {
@@ -75,14 +76,15 @@ func (u testUpdateUI) Restart() error {
 	return u.restartErr
 }
 
-func (u *testUpdateUI) ReportError(err error, options UpdateOptions) {
+func (u *testUpdateUI) ReportError(err error, update *Update, options UpdateOptions) {
 	u.errReported = err
 }
 
-func (u *testUpdateUI) ReportAction(action UpdateAction, options UpdateOptions) {
+func (u *testUpdateUI) ReportAction(action UpdateAction, update *Update, options UpdateOptions) {
 	u.actionReported = action
 	autoUpdate, _ := u.cfg.GetUpdateAuto()
 	u.autoUpdateReported = autoUpdate
+	u.updateReported = update
 }
 
 func (u testUpdateUI) UpdateOptions() UpdateOptions {
@@ -105,6 +107,7 @@ func testUpdate(uri string) *Update {
 		Name:        "Test",
 		Description: "Bug fixes",
 		InstallID:   "deadbeef",
+		RequestID:   "cafedead",
 	}
 	if uri != "" {
 		update.Asset = &Asset{
@@ -198,6 +201,9 @@ func TestUpdaterApply(t *testing.T) {
 	assert.Nil(t, ctx.errReported)
 	assert.Equal(t, ctx.actionReported, UpdateActionApply)
 	assert.True(t, ctx.autoUpdateReported)
+	require.NotNil(t, ctx.updateReported)
+	assert.Equal(t, "deadbeef", ctx.updateReported.InstallID)
+	assert.Equal(t, "cafedead", ctx.updateReported.RequestID)
 }
 
 func TestUpdaterDownloadError(t *testing.T) {
@@ -212,6 +218,8 @@ func TestUpdaterDownloadError(t *testing.T) {
 
 	require.NotNil(t, ctx.errReported)
 	assert.Equal(t, ctx.errReported.(Error).errorType, DownloadError)
+	assert.Equal(t, "deadbeef", ctx.updateReported.InstallID)
+	assert.Equal(t, "cafedead", ctx.updateReported.RequestID)
 }
 
 func TestUpdaterCancel(t *testing.T) {
