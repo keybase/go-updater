23f806a5f875e004a6de6940299194c5878df59e
Gabriel Handford <gabrielh@gmail.com>
2016-05-03 10:15:50 -0700

    Platform TODOs

--- keybase/platform_linux.go
+++ keybase/platform_linux.go
@@ -47,9 +47,11 @@ func (c context) UpdatePrompt(update updater.Update, options updater.UpdateOptio
 }
 
 func (c context) PausedPrompt() error {
+	// TODO
 	return fmt.Errorf("Unsupported")
 }
 
 func (c context) Restart() error {
-	return nil
+	// TODO
+	return fmt.Errorf("Unsupported")
 }
