42949a83174a2a5dc655fba76c8411cab6ee639c
Gabriel Handford <gabrielh@gmail.com>
2016-05-22 18:39:10 -0700

    Update go-ps

--- vendor/github.com/keybase/go-ps/process.go
+++ vendor/github.com/keybase/go-ps/process.go
@@ -1,11 +1,13 @@
-// ps provides an API for finding and listing processes in a platform-agnostic
-// way.
+// Package ps provides an API for finding and listing processes in a
+// platform-agnostic way.
 //
 // NOTE: If you're reading these docs online via GoDocs or some other system,
 // you might only see the Unix docs. This project makes heavy use of
 // platform-specific implementations. We recommend reading the source if you
 // are interested.
 package ps
+
+import "fmt"
 
 // Process is the generic interface that is implemented on every platform
 // and provides common operations for processes.
@@ -20,9 +22,12 @@ type Process interface {
 	// executable.
 	Executable() string
 
-	// Path is full path to the executable.
+	// Path is full path to the executable. The path may be unavailable if the
+	// exectuable was deleted from the system while it was still running.
 	Path() (string, error)
 }
+
+type processesFn func() ([]Process, error)
 
 // Processes returns all processes.
 //
@@ -41,3 +46,30 @@ func Processes() ([]Process, error) {
 func FindProcess(pid int) (Process, error) {
 	return findProcess(pid)
 }
+
+type matchFn func(Process) bool
+
+// findProcessesWithFn finds processes using match function.
+// If max is != 0, then we will return that max number of processes.
+func findProcessesWithFn(processesFn processesFn, matchFn matchFn, max int) ([]Process, error) {
+	processes, err := processesFn()
+	if err != nil {
+		return nil, fmt.Errorf("Error listing processes: %s", err)
+	}
+	if processes == nil {
+		return nil, nil
+	}
+	procs := []Process{}
+	for _, p := range processes {
+		if matchFn(p) {
+			procs = append(procs, p)
+		}
+		if max != 0 && len(procs) >= max {
+			break
+		}
+	}
+	return procs, nil
+}
+
+// Avoid linting error
+var _ = findProcessesWithFn
