22ffe846aea3c101b844e0f492545e508e5350b9
Gabriel Handford <gabrielh@gmail.com>
2016-05-23 17:25:15 -0700

    We are a couple branches away from master, so we'll need to wait on this

--- process/process_nix_test.go
+++ process/process_nix_test.go
@@ -1,7 +1,9 @@
 // Copyright 2015 Keybase, Inc. All rights reserved. Use of
 // this source code is governed by the included BSD license.
 
-// +build linux darwin
+// +build darwin
+
+// TODO: Include linux above after we merge into master
 
 package process
 
