c143984d363ec8578ceede41d1420f252d6548cb
Gabriel Handford <gabrielh@gmail.com>
2016-05-25 15:30:18 -0700

    Don't need to terminate other watchdogs first
    Because this is used for keybase ctl watchdog we can avoid that.

--- watchdog/watchdog.go
+++ watchdog/watchdog.go
@@ -8,14 +8,24 @@ import (
 	"os/exec"
 	"time"
 
-	"github.com/kardianos/osext"
 	"github.com/keybase/go-updater/process"
+)
+
+// ExitOn describes when a program should exit (not-restart)
+type ExitOn string
+
+const (
+	// ExitOnNone means the program should always be restarted
+	ExitOnNone ExitOn = ""
+	// ExitOnSuccess means the program should only be restart if errored
+	ExitOnSuccess ExitOn = "success"
 )
 
 // Program is a program at path with arguments
 type Program struct {
-	Path string
-	Args []string
+	Path   string
+	Args   []string
+	ExitOn ExitOn
 }
 
 // Log is the logging interface for the watchdog package
@@ -28,39 +38,11 @@ type Log interface {
 
 // Watch monitors programs and restarts them if they aren't running
 func Watch(programs []Program, restartDelay time.Duration, log Log) error {
-	execPath, err := osext.Executable()
-	if err != nil {
-		return err
-	}
-
-	// Terminate any existing watchdogs (except for ourself)
-	if err := terminateWatchdogs(execPath, time.Second, log); err != nil {
-		return err
-	}
 	// Terminate any existing programs that we are supposed to monitor
 	terminateExisting(programs, log)
 
 	// Start monitoring all the programs
 	watchPrograms(programs, restartDelay, log)
-	return nil
-}
-
-func terminateWatchdogs(execPath string, killDelay time.Duration, log Log) error {
-	procs, err := process.FindProcesses(process.NewMatcher(execPath, process.PathEqual, log), 0, 0, log)
-	if err != nil {
-		return err
-	}
-	ospid := os.Getpid()
-	log.Infof("This process is running as PID %d", ospid)
-	for _, proc := range procs {
-		pid := proc.Pid()
-		if pid != ospid {
-			log.Warningf("There is another watchdog running, terminating")
-			if err := process.TerminatePID(pid, killDelay, log); err != nil {
-				log.Warningf("Error trying to terminate another supervisor")
-			}
-		}
-	}
 	return nil
 }
 
@@ -93,8 +75,13 @@ func watchProgram(program Program, restartDelay time.Duration, log Log) {
 			log.Errorf("Error running program: %q; %s", program, err)
 		} else {
 			log.Infof("Program finished: %q", program)
+			if program.ExitOn == ExitOnSuccess {
+				log.Infof("Program configured to exit on success, not restarting")
+				break
+			}
 		}
-		if time.Since(start) < time.Minute {
+		log.Infof("Program ran for %s", time.Since(start))
+		if time.Since(start) < restartDelay {
 			log.Infof("Waiting %s before trying to start command again", restartDelay)
 			time.Sleep(restartDelay)
 		}
