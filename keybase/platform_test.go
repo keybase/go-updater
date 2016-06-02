fd1bd6c6999ef587ef84e0f4757d9d21e4466b48
Gabriel Handford <gabrielh@gmail.com>
2016-06-01 18:12:00 -0700

    Again

--- keybase/platform_test.go
+++ keybase/platform_test.go
@@ -6,11 +6,8 @@ package keybase
 import (
 	"os"
 	"path/filepath"
-	"testing"
 
 	"github.com/keybase/go-updater/command"
-	"github.com/keybase/go-updater/util"
-	"github.com/stretchr/testify/require"
 )
 
 type testConfigPlatform struct {
@@ -25,12 +22,4 @@ func (c testConfigPlatform) promptProgram() (command.Program, error) {
   			"autoUpdate": true
 			}`},
 	}, nil
-}
-
-func TestApplyNoAsset(t *testing.T) {
-	ctx := newContext(&testConfigPlatform{}, testLog)
-	tmpDir, err := util.WriteTempDir("TestApplyNoAsset.", 0700)
-	require.NoError(t, err)
-	err = ctx.Apply(testUpdate, testOptions, tmpDir)
-	require.EqualError(t, err, "No asset")
 }
