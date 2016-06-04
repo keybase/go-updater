e9218986f0d1ae40a493a29603c278201c57f5d7
Gabriel Handford <gabrielh@gmail.com>
2016-06-04 14:11:49 -0700

    Testing linting on travis

---
+++ .travis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e -u -o pipefail
+
+# To install gometalinter
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+  go get -u github.com/alecthomas/gometalinter
+  gometalinter --install --update
+  gometalinter --deadline=60s --vendor --cyclo-over=20 --dupl-threshold=100 ./...
+fi
