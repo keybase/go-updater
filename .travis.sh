caa1518642787e626a829c390316abee1056fd07
Gabriel Handford <gabrielh@gmail.com>
2016-06-04 15:56:00 -0700

    Fix path

--- .travis.sh
+++ .travis.sh
@@ -6,5 +6,5 @@ set -e -u -o pipefail
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
   go get -u github.com/alecthomas/gometalinter
   gometalinter --install --update
-  gometalinter --deadline=300s --vendor --cyclo-over=20 --dupl-threshold=100 ./...
+  gometalinter --deadline=300s --vendor --cyclo-over=20 --dupl-threshold=100 $HOME/gopath/src/github.com/keybase/go-updater/...
 fi
