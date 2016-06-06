3314b3670de1f1a1522d82feebd25297606f09d5
Gabriel Handford <gabrielh@gmail.com>
2016-06-05 21:38:37 -0700

    Debugging

--- .travis.sh
+++ .travis.sh
@@ -4,11 +4,10 @@ set -e -u -o pipefail
 
 # To install gometalinter
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-  echo "Installing gometalinter"
   go get -u github.com/alecthomas/gometalinter
   gometalinter --install --update
   echo "Running gometalinter"
   # $GOPATH/src/github.com/keybase/go-updater
+  echo "Current directory: `pwd`"
   gometalinter --deadline=300s --vendor --cyclo-over=20 --dupl-threshold=100 ./...
-  echo "Status: $?"
 fi
