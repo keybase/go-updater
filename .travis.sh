0b0112143e277dd1767349a6bb88882520c0f60d
Gabriel Handford <gabrielh@gmail.com>
2016-06-24 12:46:05 -0700

    Script it

--- .travis.sh
+++ .travis.sh
@@ -1,13 +1,33 @@
-#!/usr/bin/env s
+#!/usr/bin/env bash
 
-set -e -u -o pipefail
+set -f -u -e
+
+echo "Current directory: `pwd`"
+
+gopath=${GOPATH:-"$HOME/gopath"}
 
 # To install gometalinter
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
   go get -u github.com/alecthomas/gometalinter
   gometalinter --install --update
   echo "Running gometalinter"
-  echo "Current directory: `pwd`"
   gometalinter --deadline=300s --vendor --cyclo-over=20 --dupl-threshold=100 ./...
-  echo "Finished"
 fi
+
+repo="github.com/keybase/go-updater"
+cd "$gopath/src/$repo"
+
+godirs=`go list ./... | grep -v /vendor/`
+for i in $godirs; do
+  if [ "$i" = "$repo" ]; then
+    echo "$repo..."
+    #go test -timeout 5m -coverprofile="main.coverprofile"
+  else
+    package=${i##*/}
+    echo "$repo ($package)..."
+    #go test -timeout 5m -coverprofile="$package.coverprofile" ./"$package"
+  fi
+done
+
+"$gopath/bin/gover"
+"$gopath/bin/goveralls" -coverprofile=gover.coverprofile -service=travis-ci
