8bb5ea0e83e739ad0652a7db08d326e5c0dc0fe6
Steve Sanders <steve@keyba.se>
2018-03-08 08:08:35 -0800

    Remove all the stuff that overrides silent update on driver upgrade (#149)
    * just check for dokan1.dll
    
    * remove quietcheck
    
    * go 1.8.4 -> 1.9.2
    
    * remove more driver stuff
    
    * add explicit logging arguments for .msi

--- keybase/platform_windows.go
+++ keybase/platform_windows.go
@@ -7,6 +7,8 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"os"
+	"path"
 	"path/filepath"
 	"runtime"
 	"strings"
@@ -33,7 +35,6 @@ type guid struct {
 // F1B32785-6FBA-4FCF-9D55-7B8E7F157091
 var (
 	folderIDLocalAppData = guid{0xF1B32785, 0x6FBA, 0x4FCF, [8]byte{0x9D, 0x55, 0x7B, 0x8E, 0x7F, 0x15, 0x70, 0x91}}
-	folderIDSystem       = guid{0x1AC14E77, 0x02E7, 0x4E5D, [8]byte{0xB7, 0x44, 0x2E, 0xB1, 0xAE, 0x51, 0x98, 0xB7}}
 )
 
 var (
@@ -140,144 +141,7 @@ func (c config) notifyProgram() string {
 }
 
 func (c *context) BeforeUpdatePrompt(update updater.Update, options updater.UpdateOptions) error {
-	// This is the check whether to override an auto update. Unnecessary if
-	// auto update is not on in the first place.
-	_, auto := c.config.GetUpdateAuto()
-	if auto && !c.config.GetUpdateAutoOverride() {
-		dokan86 := ""
-		dokan64 := ""
-		for _, prop := range update.Props {
-			switch prop.Name {
-			case "DokanProductCodeX64":
-				dokan64 = prop.Value
-			case "DokanProductCodeX86":
-				dokan86 = prop.Value
-			}
-		}
-		if canBeSilent, _ := CheckCanBeSilent(dokan64, dokan86, c.log, CheckRegistryUninstallCode); !canBeSilent {
-			c.config.SetUpdateAutoOverride(true)
-		}
-	}
 	return nil
-}
-
-func detectDokanDll(log Log) bool {
-	dir, err := getDataDir(folderIDSystem)
-	if err != nil {
-		log.Infof("detectDokanDll error getting system directory: %v", err)
-		return false
-	}
-
-	exists, _ := util.FileExists(filepath.Join(dir, "dokan1.dll"))
-	log.Infof("detectDokanDll: returning %v", exists)
-	return exists
-}
-
-type regUninstallGetter func(string, Log) bool
-
-// CheckCanBeSilent - Takes an incoming installer's driver
-// uninstall codes, which will be in the registry if the same
-// version is already present.
-// If it's not installed, check if dokan1.dll is present, which will indicate
-// some other version is present.
-func CheckCanBeSilent(dokanCodeX86 string, dokanCodeX64 string, log Log, regFunc regUninstallGetter) (bool, error) {
-	codeFound := false
-	var err error
-
-	// Also look in the registry whether a reboot is pending from a previous installer.
-	// Not finding the key is not an error.
-	if rebootPending, err := checkRebootPending(false, log); rebootPending && err == nil {
-		return false, nil
-	}
-
-	if rebootPending, err := checkRebootPending(true, log); rebootPending && err == nil {
-		return false, nil
-	}
-
-	if regFunc(dokanCodeX86, log) || regFunc(dokanCodeX64, log) {
-		codeFound = true
-	} else {
-		// If we don't find our dokan installed, see whether another one is,
-		// and allow silent update if not.
-		if !detectDokanDll(log) {
-			codeFound = true
-		}
-	}
-
-	log.Infof("CheckCanBeSilent: returning %v", codeFound)
-	return codeFound, err
-}
-
-// CheckRegistryUninstallCode is exported so a little standalone
-// test utility can use it
-func CheckRegistryUninstallCode(productID string, log Log) bool {
-	log.Infof("CheckCanBeSilent: Searching registry for %s", productID)
-	if productID == "" {
-		log.Info("CheckCanBeSilent: Empty product ID, returning false")
-		return false
-	}
-	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\`+productID, registry.QUERY_VALUE|registry.WOW64_64KEY)
-	defer util.Close(k)
-	if err == nil {
-		log.Infof("CheckCanBeSilent: Found %s", productID)
-		return true
-	}
-	return false
-}
-
-// Read all the runonce subkeys and find the one with "Keybase" in the name.
-func checkRebootPending(wow64 bool, log Log) (bool, error) {
-	var access uint32 = registry.ENUMERATE_SUB_KEYS | registry.QUERY_VALUE
-	if wow64 {
-		access = access | registry.WOW64_32KEY
-	}
-
-	k, err := registry.OpenKey(registry.CURRENT_USER, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\RunOnce", access)
-	if err != nil {
-		log.Errorf("Error opening RunOnce subkeys: %s", err)
-		return false, err
-	}
-	defer util.Close(k)
-
-	params, err := k.ReadValueNames(0)
-	if err != nil {
-		log.Errorf("Can't ReadSubKeyNames %#v", err)
-		return false, err
-	}
-
-	for _, param := range params {
-		val, _, err := k.GetStringValue(param)
-
-		if err != nil {
-			log.Warningf("Error getting string value for %s: %s", param, err)
-			continue
-		}
-		if strings.Contains(val, "Keybase") {
-			return true, nil
-		}
-	}
-
-	// Check for a Dokan reboot pending
-	k2, err := registry.OpenKey(registry.LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Control\\Session Manager", access)
-	if err != nil {
-		log.Errorf("Error opening Session Manager key: %s", err)
-		return false, err
-	}
-	defer util.Close(k2)
-	vals, _, err := k2.GetStringsValue("PendingFileRenameOperations")
-	if err != nil {
-		// This is normal if no reboot is pending
-		log.Errorf("Error getting PendingFileRenameOperations: %s", err)
-	} else {
-		for _, val := range vals {
-			if strings.Contains(strings.ToLower(val), "dokan") {
-				log.Info("Found Dokan reboot pending")
-				return true, nil
-			}
-		}
-	}
-
-	return false, nil
 }
 
 func (c config) promptProgram() (command.Program, error) {
@@ -344,7 +208,8 @@ func (c context) Apply(update updater.Update, options updater.UpdateOptions, tmp
 	if update.Asset == nil || update.Asset.LocalPath == "" {
 		return fmt.Errorf("No asset")
 	}
-	var args []string
+
+	args := []string{"/log", path.Join(os.TempDir(), "KeybaseApps_"+time.Now().Format(time.RFC3339)) + ".log"}
 	auto, _ := c.config.GetUpdateAuto()
 	if auto && !c.config.GetUpdateAutoOverride() {
 		args = append(args, "/quiet", "/norestart")
--- keybase/platform_windows_test.go
+++ keybase/platform_windows_test.go
@@ -63,20 +63,3 @@ func TestApplyAsset(t *testing.T) {
 	err = ctx.Apply(update, updater.UpdateOptions{}, tmpDir)
 	require.NoError(t, err)
 }
-
-const legit64Code = "{65A3A964-3DC3-0100-0000-160621082245}"
-const legit86Code = "{65A3A986-3DC3-0100-0000-160621082245}"
-const mismatchedCode = "{65A3A986-3DC3-0100-0000-160621082244}"
-
-func testCheckCanBeSlient(t *testing.T, dokan86Code string, dokan64Code string, testcode string) bool {
-	result, err := CheckCanBeSilent(dokan86Code, dokan64Code, testLog, func(s string, l Log) bool { return s == testcode })
-	require.NoError(t, err)
-	return result
-}
-
-func TestSearchInstallerLayout(t *testing.T) {
-	assert.Equal(t, testCheckCanBeSlient(t, legit86Code, legit64Code, legit64Code), true)
-	assert.Equal(t, testCheckCanBeSlient(t, legit86Code, legit64Code, legit86Code), true)
-	// This is now true because there's no dokan1.dll
-	assert.Equal(t, testCheckCanBeSlient(t, legit86Code, legit64Code, mismatchedCode), true)
-}
