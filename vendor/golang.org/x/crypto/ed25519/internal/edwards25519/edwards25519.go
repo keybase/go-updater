9e6fecde5d6d867aae04ac483139cb50fe113d61
Frederick Akalin <akalin@keyba.se>
2018-03-23 21:32:53 -0700

    Revendor correct ed25519 library

--- vendor/golang.org/x/crypto/ed25519/internal/edwards25519/edwards25519.go
+++ vendor/golang.org/x/crypto/ed25519/internal/edwards25519/edwards25519.go
@@ -1,13 +1,10 @@
-// Copyright 2013 The Go Authors. All rights reserved.
+// Copyright 2016 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package edwards25519 implements operations in GF(2**255-19) and on an
-// Edwards curve that is isomorphic to curve25519. See
-// http://ed25519.cr.yp.to/.
 package edwards25519
 
-// This code is a port of the public domain, "ref10" implementation of ed25519
+// This code is a port of the public domain, “ref10” implementation of ed25519
 // from SUPERCOP.
 
 // FieldElement represents an element of the field GF(2^255 - 19).  An element
@@ -373,7 +370,7 @@ func FeCombine(h *FieldElement, h0, h1, h2, h3, h4, h5, h6, h7, h8, h9 int64) {
 // 10 of them are 2-way parallelizable and vectorizable.
 // Can get away with 11 carries, but then data flow is much deeper.
 //
-// With tighter constraints on inputs can squeeze carries into int32.
+// With tighter constraints on inputs, can squeeze carries into int32.
 func FeMul(h, f, g *FieldElement) {
 	f0 := int64(f[0])
 	f1 := int64(f[1])
@@ -928,7 +925,8 @@ func GeDoubleScalarMultVartime(r *ProjectiveGroupElement, a *[32]byte, A *Extend
 	}
 }
 
-// equal returns 1 if b == c and 0 otherwise.
+// equal returns 1 if b == c and 0 otherwise, assuming that b and c are
+// non-negative.
 func equal(b, c int32) int32 {
 	x := uint32(b ^ c)
 	x--
