a19b33007a0d444386cc4e2fed3b782229a140c1
Joshua Blum <jblum18@gmail.com>
2019-08-27 17:01:28 -0400

    move testConfigPausedPrompt to darwin, unused elsewhere

--- keybase/context_darwin_test.go
+++ keybase/context_darwin_test.go
@@ -6,11 +6,48 @@
 package keybase
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/keybase/go-updater"
+	"github.com/keybase/go-updater/command"
 	"github.com/stretchr/testify/require"
 )
+
+type testConfigPausedPrompt struct {
+	config
+	inUse bool
+	force bool
+}
+
+func (c testConfigPausedPrompt) promptProgram() (command.Program, error) {
+	if c.force {
+		return command.Program{
+			Path: filepath.Join(os.Getenv("GOPATH"), "bin", "test"),
+			Args: []string{"echo", `{"button": "Force update"}`},
+		}, nil
+	}
+	return command.Program{
+		Path: filepath.Join(os.Getenv("GOPATH"), "bin", "test"),
+		Args: []string{"echo", `{"button": "Try again later"}`},
+	}, nil
+}
+
+func (c testConfigPausedPrompt) keybasePath() string {
+	if c.inUse {
+		return filepath.Join(os.Getenv("GOPATH"), "src/github.com/keybase/go-updater/test/keybase-check-in-use-true.sh")
+	}
+	return filepath.Join(os.Getenv("GOPATH"), "src/github.com/keybase/go-updater/test/keybase-check-in-use-false.sh")
+}
+
+func (c testConfigPausedPrompt) updaterOptions() updater.UpdateOptions {
+	return updater.UpdateOptions{}
+}
+
+func (c testConfigPausedPrompt) destinationPath() string {
+	return "/Applications/Test.app"
+}
 
 func TestContextCheckInUse(t *testing.T) {
 	// In use, force
--- keybase/context_test.go
+++ keybase/context_test.go
@@ -9,7 +9,6 @@ import (
 	"testing"
 
 	"github.com/keybase/go-updater"
-	"github.com/keybase/go-updater/command"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -80,38 +79,4 @@ func TestContextVerifyBadSignature(t *testing.T) {
 	ctx := testContext(t)
 	err := ctx.Verify(testContextUpdate(testMessagePath, "BEGIN KEYBASE SALTPACK DETACHED SIGNATURE. END KEYBASE SALTPACK DETACHED SIGNATURE."))
 	require.Error(t, err)
-}
-
-type testConfigPausedPrompt struct {
-	config
-	inUse bool
-	force bool
-}
-
-func (c testConfigPausedPrompt) promptProgram() (command.Program, error) {
-	if c.force {
-		return command.Program{
-			Path: filepath.Join(os.Getenv("GOPATH"), "bin", "test"),
-			Args: []string{"echo", `{"button": "Force update"}`},
-		}, nil
-	}
-	return command.Program{
-		Path: filepath.Join(os.Getenv("GOPATH"), "bin", "test"),
-		Args: []string{"echo", `{"button": "Try again later"}`},
-	}, nil
-}
-
-func (c testConfigPausedPrompt) keybasePath() string {
-	if c.inUse {
-		return filepath.Join(os.Getenv("GOPATH"), "src/github.com/keybase/go-updater/test/keybase-check-in-use-true.sh")
-	}
-	return filepath.Join(os.Getenv("GOPATH"), "src/github.com/keybase/go-updater/test/keybase-check-in-use-false.sh")
-}
-
-func (c testConfigPausedPrompt) updaterOptions() updater.UpdateOptions {
-	return updater.UpdateOptions{}
-}
-
-func (c testConfigPausedPrompt) destinationPath() string {
-	return "/Applications/Test.app"
 }
